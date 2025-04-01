5bfe68c2258724c2545bfcd193ec187c2997aa24
Tom Bischoff <to.bischoff@ostfalia.de>
2025-04-01 14:06:18 +0200

    test property finished

--- Codegenerator/src/codegenerator/templates/PropertyTemplate.xtend
+++ Codegenerator/src/codegenerator/templates/PropertyTemplate.xtend
@@ -6,9 +6,31 @@ import org.eclipse.uml2.uml.AggregationKind
 import org.eclipse.uml2.uml.Property
 
 class PropertyTemplate implements Template<Property> {
-	
+
 	override generateCode(CodegenInterface it, Property umlProperty, String context) {
-		// TODO: Aufgabe 1
+		// Aufgabe 1
+		var typeName = ""
+		var name = ""
+
+		if (context == "attribute") {
+			name = generate(umlProperty, "name")
+			if (umlProperty.type !== null) {
+				typeName = generate(umlProperty.type, "name")
+			} else {
+				typeName = "void*"
+			}
+			
+			//complex typed property, owned complex typed property
+			if (umlProperty.type !== null && umlProperty.type.eClass.name == "Class" && !(umlProperty.aggregation == AggregationKind.COMPOSITE_LITERAL)){
+				typeName = typeName + "*"
+			}		
+			
+		} else {
+			println("PropertyTemplate: unknown context")
+		}
+
+		// Rückgabe der Werte
+		'''«typeName» «name»;'''
 	}
-	
+
 }
--- Codegenerator/xtend-gen/codegenerator/templates/PropertyTemplate.java
+++ Codegenerator/xtend-gen/codegenerator/templates/PropertyTemplate.java
@@ -2,12 +2,44 @@ package codegenerator.templates;
 
 import codegenerator.CodegenInterface;
 import codegenerator.Template;
+import com.google.common.base.Objects;
+import org.eclipse.uml2.uml.AggregationKind;
 import org.eclipse.uml2.uml.Property;
+import org.eclipse.uml2.uml.Type;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.xbase.lib.InputOutput;
 
 @SuppressWarnings("all")
 public class PropertyTemplate implements Template<Property> {
   @Override
   public String generateCode(final CodegenInterface it, final Property umlProperty, final String context) {
-    return null;
+    String _xblockexpression = null;
+    {
+      String typeName = "";
+      String name = "";
+      boolean _equals = Objects.equal(context, "attribute");
+      if (_equals) {
+        name = it.generate(umlProperty, "name");
+        Type _type = umlProperty.getType();
+        boolean _tripleNotEquals = (_type != null);
+        if (_tripleNotEquals) {
+          typeName = it.generate(umlProperty.getType(), "name");
+        } else {
+          typeName = "void*";
+        }
+        if ((((umlProperty.getType() != null) && Objects.equal(umlProperty.getType().eClass().getName(), "Class")) && (!Objects.equal(umlProperty.getAggregation(), AggregationKind.COMPOSITE_LITERAL)))) {
+          typeName = (typeName + "*");
+        }
+      } else {
+        InputOutput.<String>println("PropertyTemplate: unknown context");
+      }
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append(typeName);
+      _builder.append(" ");
+      _builder.append(name);
+      _builder.append(";");
+      _xblockexpression = _builder.toString();
+    }
+    return _xblockexpression;
   }
 }
